a2149db484909c31f14c0750e4f9bf4413a581b7
Riukim <m.cominelli29@gmail.com>
2024-05-08 10:49:16 +0200

    Make sign-up an atomic transaction with dwolla, fix onSubmit function for sign-up, fix types for SignupParams

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -41,7 +41,20 @@ const AuthForm = ({ type }: { type: string }) => {
       // Sign up with Appwrite & create a plaid link token
 
       if (type === "sign-up") {
-        const newUser = await signUp(data)
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password
+        }
+
+        const newUser = await signUp(userData)
 
         setUser(newUser)
       }
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,12 +3,12 @@
 import { ID } from "node-appwrite"
 import { createAdminClient, createSessionClient } from "../appwrite"
 import { cookies } from "next/headers"
-import { encryptId, parseStringify } from "../utils"
+import { encryptId, extractCustomerIdFromUrl, parseStringify } from "../utils"
 import { CountryCode, ProcessorTokenCreateRequest, ProcessorTokenCreateRequestProcessorEnum, Products } from "plaid"
 
 import { plaidClient } from "../plaid"
 import { revalidatePath } from "next/cache"
-import { addFundingSource } from "./dwolla.actions"
+import { addFundingSource, createDwollaCustomer } from "./dwolla.actions"
 
 const {
   APPWRITE_DATABASE_ID: DATABASE_ID,
@@ -29,18 +29,45 @@ export const signIn = async ({email, password}: signInProps) => {
   }
 }
 
+// atomic transaction, can't create user and not add user to database
+// or can't add user to database and not connect to Plaid
 export const signUp = async (userData: SignUpParams) => {
-  const {email, password, firstName, lastName} = userData
+  const { email, password, firstName, lastName } = userData
+  
+  let newUserAccount
 
   try {
     // Create user account
-    const { account } = await createAdminClient()
+    const { account, database } = await createAdminClient()
 
-    const newUserAccount = await account.create(
+    newUserAccount = await account.create(
       ID.unique(),
       email,
       password,
       `${firstName} ${lastName}`
+    )
+
+    if (!newUserAccount) throw new Error("Error creating user")
+    
+    const dwollaCustomerUrl = await createDwollaCustomer({
+      ...userData,
+      type: "personal"
+    })
+
+    if (!dwollaCustomerUrl) throw new Error("Error creating Dwolla customer")
+    
+    const dwollaCustomerId = extractCustomerIdFromUrl(dwollaCustomerUrl)
+
+    const newUser = await database.createDocument(
+      DATABASE_ID!,
+      USER_COLLECTION_ID!,
+      ID.unique(),
+      {
+        ...userData,
+        userId: newUserAccount.$id,
+        dwollaCustomerId,
+        dwollaCustomerUrl
+      }
     )
 
     const session = await account.createEmailPasswordSession(email, password)
@@ -52,7 +79,7 @@ export const signUp = async (userData: SignUpParams) => {
       secure: true,
     })
 
-    return parseStringify(newUserAccount)
+    return parseStringify(newUser)
   } catch (error) {
     console.log("Error ", error)
   }
